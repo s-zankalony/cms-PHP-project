92c05055b3f19235f8ccbf82ea08653daaf8fa43
Sameh El Zankalony <zankalony@gmail.com>
2024-05-09 00:29:30 +0300

    completing update post functionality

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -19,6 +19,54 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
   $post_status = $row['post_status'];
   $post_comments = $row['post_comment_count'];
 }
+
+if (isset($_POST['update_post'])) {
+  $post_title = $_POST['title'];
+  $post_author = $_POST['author'];
+  $post_category_id = $_POST['category_name'];
+  $post_status = $_POST['post_status'];
+  $post_image = $_FILES['image']['name'];
+  $post_image_temp = $_FILES['image']['tmp_name'];
+  $post_tags = $_POST['post_tags'];
+  $post_content = mysqli_real_escape_string($connection, $_POST['post_content']);
+  $post_date = date('Y-m-d H:i:s');
+  $post_comment_count = 4;
+
+  move_uploaded_file($post_image_temp, "../images/$post_image");
+
+
+
+  if (empty($post_image)) {
+    $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+    $select_image = mysqli_query($connection, $query);
+    confirmQuery($select_image);
+
+    while ($row = mysqli_fetch_array($select_image)) {
+      $post_image = $row['post_image'];
+    }
+  }
+
+
+  $query = "UPDATE posts SET ";
+  $query .= "post_title = '{$post_title}', ";
+  $query .= "post_category_id = '{$post_category_id}', ";
+  $query .= "post_date = now(), ";
+  $query .= "post_author = '{$post_author}', ";
+  $query .= "post_status = '{$post_status}', ";
+  $query .= "post_tags = '{$post_tags}', ";
+  $query .= "post_content = '{$post_content}', ";
+  $query .= "post_image = '{$post_image}' ";
+  $query .= "WHERE post_id = {$the_post_id}";
+  $update_post = mysqli_query($connection, $query);
+  confirmQuery($update_post);
+  echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
+
+}
+
+if (isset($_POST['cancel'])) {
+  header("Location: posts.php");
+}
+
 ?>
 
 
@@ -39,9 +87,9 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
       while ($row = mysqli_fetch_assoc($select_categories)) {
         $cat_name = $row['cat_title'];
-        // $cat_id = $row['cat_id'];
-      
-        echo "<option value='{$cat_name}'>{$cat_name}</option>";
+        $cat_id = $row['cat_id'];
+
+        echo "<option value='{$cat_id}'>{$cat_name}</option>";
 
       }
 
@@ -68,8 +116,9 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
 
   <div class="form-group">
-    <label for="post_image">Post Image</label>
-    <img src="../images/<?php echo $post_image ?>" width="100" alt="image">
+
+    <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+    <input type="file" name="image">
   </div>
 
   <div class="form-group">
@@ -85,7 +134,8 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
 
   <div class="form-group">
-    <input class="btn btn-primary" type="submit" name="create_post" value="Update Post">
+    <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
+    <input class="btn btn-primary" type="submit" name="cancel" value="Cancel">
   </div>
 
 
