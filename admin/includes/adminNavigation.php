79694ac47efbf3298de9d34c8738c1a61e79dda7
Sameh El Zankalony <zankalony@gmail.com>
2024-05-11 00:47:44 +0300

    adding category page, each post has its own page now

--- admin/includes/adminNavigation.php
+++ admin/includes/adminNavigation.php
@@ -33,7 +33,7 @@
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
       <li>
-        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
       </li>
 
       <li class="<?php echo (basename($_SERVER['REQUEST_URI']) === 'posts.php' ? 'active' : ''); ?>">
